18a991b6e445476f1aea61f39c3929ae80a5bfcd
Mina <cccoldatlas@gmail.com>
2020-10-27 17:28:43 +0200

    add player move and player move update

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,13 @@
 class Board
-  attr_reader :inputs
+  attr_reader :moves
   attr_accessor :board
 
   def new
-    @inputs = 0
+    @moves = 0
     @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
   end
 
-  def display
+  def show_board
 
     puts '╔═══╦═══╦═══╗'
     puts "║ #{@board[0]} ║ #{@board[1]} ║ #{@board[2]} ║"
@@ -19,16 +19,32 @@ class Board
 
   end
 
+   def player_move(square)
+    chosen_square = @board[0] if square == 1
+    chosen_square = @board[1] if square == 2
+    chosen_square = @board[2] if square == 3
+    chosen_square = @board[3] if square == 4
+    chosen_square = @board[4] if square == 5
+    chosen_square = @board[5] if square == 6
+    chosen_square = @board[6] if square == 7
+    chosen_square = @board[7] if square == 8
+    chosen_square = @board[8] if square == 9
+    chosen_square
+  end
 
-
-
-
-
-
-
-
-
-
+  def player_move_update(square ,chosen_square)
+    @board[0] = chosen_square if square == 1
+    @board[1] = chosen_square if square == 2
+    @board[2] = chosen_square if square == 3
+    @board[3] = chosen_square if square == 4
+    @board[4] = chosen_square if square == 5
+    @board[5] = chosen_square if square == 6
+    @board[6] = chosen_square if square == 7
+    @board[7] = chosen_square if square == 8
+    @board[8] = chosen_square if square == 9
+    show_board
+    moves += 1
+  end
 
 
 
